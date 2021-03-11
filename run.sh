38901b3944d080df5b6934591d92ee764632670d
root <michael@cadot.info>
2021-03-11 14:55:30 +0100

    test:cdn and css and babel

---
+++ run.sh
@@ -0,0 +1,8 @@
+result=${PWD##*/}
+res=${PWD}
+docker kill $result
+docker rm $result
+docker run -d -p 80:80  \
+    --name $result \
+    -v $res:/var/www/html \
+cadotinfo/docker-php7.4-apache-yarn-composer
