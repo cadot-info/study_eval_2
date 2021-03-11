920d18a13bb757952818c4f762ceee5acff8788f
root <michael@cadot.info>
2021-03-11 19:51:17 +0100

    feat: dice show on roll and round ok

--- app.js
+++ app.js
@@ -4,3 +4,23 @@ const new_game = () => {
     $('.total').text('0');
     $('.resultat-nombre').text('0');
 }
+var ROUND = [0, 0]
+var GLOBAL = [0, 0]
+const rollDice = () => {
+    var positionx = [0, 0, -130, 100, -130, 100];
+    var positiony = [0, 100, 100, 100, 0, 0];
+    // on affiche le dé
+    $('#imgde').show()
+    var de = Math.floor((Math.random() * 6) + 1);
+    $('#imgde').css('background-position-x', positionx[de - 1] + 'px').css('background-position-y', positiony[de - 1] + 'px')
+    if (de != 1) {
+        ROUND[1] += de;
+        $($('.resultat-nombre')[0]).text(ROUND[1]);
+    }
+    else {
+        ROUND[1] = 0;
+        $($('.resultat-nombre')[0]).text(0);
+    }
+
+}
+$('#imgde').show();
